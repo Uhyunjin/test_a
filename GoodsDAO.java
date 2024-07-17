84232129572e0b73e010fe3a0d9dca4cfd2f4504
유현진 <uhyeonjin0619@gmail.com>
2024-07-17 16:51:40 +0900

    create project

---
+++ GoodsDAO.java
@@ -0,0 +1,9 @@
+class GoodsDAO 
+{
+	public static void insert(){
+		System.out.println("insert OK");
+	}
+	public static void update(){
+		System.out.println("update OK");
+	}
+}
---
+++ Hello.java
@@ -0,0 +1,9 @@
+class Hello 
+{
+	public static void main(String[] args) 
+	{
+		GoodsDAO gDao=new GoodsDAO();
+		gDao.insert();
+		gDao.update();
+	}
+}
